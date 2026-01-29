a5a2d3016af4415a268bb68391ced8e0224c2a7d
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-29 14:49:03 +0100

    Add DAL Project

---
+++ ProjectLibrary.DAL/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectLibrary.DAL.Entities
+{
+    public class Book
+    {
+        public Guid BookId { get; set; }
+        //Structure d'un Guid : ########-####-####-####-#############
+
+        public string Title { get; set; }
+        public string? Author { get; set; }
+        public string? ISBN { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime RegisteredDate { get; set; }
+        public DateTime? DisabledDate { get; set; }
+    }
+}
---
+++ ProjectLibrary.DAL/Services/BookService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using ProjectLibrary.DAL.Entities;
+using ProjectLibrary.DAL.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ProjectLibrary.DAL.Services
+{
+    public class BookService
+    {
+        private readonly string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProjectLibrary;Integrated Security=True";
+        public IEnumerable<Book> Get()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SP_Book_Get_All";
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            yield return reader.ToBook();
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+        public Book Get(Guid bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid Create(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid bookId, Book newData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid bookId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
