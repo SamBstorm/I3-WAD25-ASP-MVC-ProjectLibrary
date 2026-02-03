f34aa31022c57b08b0b989e804efc4b30d2a1d50
Samuel Legrain <samuel.legrain@bstorm.be>
2026-02-03 14:27:24 +0100

    Add IUserProileRepository<>

---
+++ ProjectLibrary.Common/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectLibrary.Common.Repositories
+{
+    public interface IUserProfileRepository<TUserProfile>
+    {
+        public IEnumerable<TUserProfile> Get();
+        public TUserProfile Get(Guid id);
+        public Guid Create(TUserProfile entity);
+        public void Update(Guid id, TUserProfile entity);
+        public void Delete(Guid id);
+    }
+}
