6d128f90cc17a9f1ff597d02770135896f72d204
Samuel Legrain <samuel.legrain@bstorm.be>
2026-02-04 14:30:37 +0100

    Fix RegularExpression Login & Register

--- ProjectLibrary.ASPMVC/Models/Auth/LoginForm.cs
+++ ProjectLibrary.ASPMVC/Models/Auth/LoginForm.cs
@@ -12,7 +12,7 @@ namespace ProjectLibrary.ASPMVC.Models.Auth
         [DisplayName("Mot de passe : ")]
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&=\-+])[a-z-AZ\d@$!%*?&=\-+]{8,64}$", ErrorMessage = "Le mot de passe ne correspond pas à la sécurité minimale requise.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&=\-+])[a-zA-Z\d@$!%*?&=\-+]{8,64}$", ErrorMessage = "Le mot de passe ne correspond pas à la sécurité minimale requise.")]
         [MinLength(8, ErrorMessage = "Le mot de passe doit avoir au minimum 8 caractères.")]
         [MaxLength(64, ErrorMessage = "Le mot de passe doit avoir au maximum 64 caractères.")]
         public string Password { get; set; }
