8cd0064a2a0be9b13a6f8be82d1c0a78b1e7b293
Samuel Legrain <samuel.legrain@bstorm.be>
2026-02-09 11:17:20 +0100

    Add Demo ManyToMany  Add/Remove Category

---
+++ ProjectLibrary.ASPMVC/Controllers/CategoryController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectLibrary.BLL.Entities;
+using ProjectLibrary.Common.Repositories;
+
+namespace ProjectLibrary.ASPMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository<BLL.Entities.Category> _categoryService;
+        private readonly IBookRepository<BLL.Entities.Book> _bookService;
+
+        public CategoryController(
+            ICategoryRepository<Category> categoryService, 
+            IBookRepository<Book> bookService)
+        {
+            _categoryService = categoryService;
+            _bookService = bookService;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(Index),"Home");
+        }
+
+        [HttpPost]
+        public IActionResult CategoryManager(Guid bookId, string submit)
+        {
+            if(submit == "+")
+            {
+                return RedirectToAction("AddCategory", "Category", new {bookId});
+            }
+            else if(submit == "-")
+            {
+                return RedirectToAction("RemoveCategory", "Category", new { bookId });
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult AddCategory(Guid bookId)
+        {
+            IEnumerable<BLL.Entities.Category> bookCategories = _categoryService.GetByBook(bookId);
+            IEnumerable<BLL.Entities.Category> allCategories = _categoryService.Get();
+            IEnumerable<BLL.Entities.Category> model = allCategories.ExceptBy(bookCategories.Select(bc => bc.CategoryId), cat => cat.CategoryId);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Guid bookId, int categoryId)
+        {
+            try
+            {
+                _bookService.AddCategory(bookId, categoryId);
+                return RedirectToAction("Details", "Book", new { id = bookId });
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
+        public IActionResult RemoveCategory(Guid bookId)
+        {
+            IEnumerable<BLL.Entities.Category> model = _categoryService.GetByBook(bookId);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult RemoveCategory(Guid bookId, int categoryId)
+        {
+            try
+            {
+                _bookService.RemoveCategory(bookId, categoryId);
+                return RedirectToAction("Details", "Book", new { id = bookId });
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+    }
+}
